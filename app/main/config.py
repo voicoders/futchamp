ae2b22a3c53f905cb13cd1306599d13b9aa9da8c
jpccarmo <jpccarmo@gmail.com>
2019-04-01 22:56:37 +0100

    Set dev connection string

--- app/main/config.py
+++ app/main/config.py
@@ -12,7 +12,7 @@ class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'futchamp_dev_main.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://dnmqzsbokuhguc:5ce63360e0fb3aac055681e0dfccf49b9e8e5b101768606d5e34d65049a08806@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/d8circ9r6b54od'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://testuser:testuser@localhost/flask_boilerplate_main'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
