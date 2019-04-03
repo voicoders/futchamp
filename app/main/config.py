8300c4f9500724d3a935e065e1f5c31a7e3964fd
jpccarmo <jpccarmo@gmail.com>
2019-04-03 21:19:54 +0100

    Set connection string from environment variable DATABASE_URL

--- app/main/config.py
+++ app/main/config.py
@@ -3,6 +3,7 @@ import os
 # postgres_local_base = os.environ['DATABASE_URL']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_URL = os.environ['DATABASE_URL']
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
@@ -12,8 +13,8 @@ class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://dnmqzsbokuhguc:5ce63360e0fb3aac055681e0dfccf49b9e8e5b101768606d5e34d65049a08806@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/d8circ9r6b54od'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://testuser:testuser@localhost/flask_boilerplate_main'
+    # SQLALCHEMY_DATABASE_URI = 'postgres://dnmqzsbokuhguc:5ce63360e0fb3aac055681e0dfccf49b9e8e5b101768606d5e34d65049a08806@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/d8circ9r6b54od'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
